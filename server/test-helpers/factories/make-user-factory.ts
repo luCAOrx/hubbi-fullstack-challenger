0a9c69c790411a2682fc95bfbb62bc99ec4ee080
luCAOrx <lucaorx@protonmail.com>
2025-01-26 23:03:40 -0300

    test(Helpers): Add method to create sale in domain layer

---
+++ server/test-helpers/factories/make-user-factory.ts
@@ -0,0 +1,51 @@
+import { randomUUID } from "node:crypto";
+
+import { Sale, Status } from "@domain/entities/sale/sale";
+import { CreateSaleUseCase } from "@domain/use-cases/create-sale/create-sale-use-case";
+
+import { InMemorySaleDatabase } from "../in-memory-database/in-memory-sale-database";
+
+interface SaleProps {
+  name: string;
+  status: Status;
+  products: { public_id: string; name: string }[];
+}
+
+type Override = Partial<SaleProps>;
+
+export class MakeSaleFactory {
+  public async toDomain({
+    inMemoryDatabase,
+    override,
+  }: {
+    inMemoryDatabase: InMemorySaleDatabase;
+    override?: Override;
+  }): Promise<Sale> {
+    const inMemorySaleDatabase = new InMemorySaleDatabase();
+    const createSaleUseCase = new CreateSaleUseCase(inMemorySaleDatabase);
+
+    const { sale } = await createSaleUseCase.execute({
+      name: "Produtos de limpeza",
+      status: "Pendente",
+      products: [
+        {
+          public_id: randomUUID(),
+          name: "Sabão em pó",
+        },
+        {
+          public_id: randomUUID(),
+          name: "Sabão líquido",
+        },
+        {
+          public_id: randomUUID(),
+          name: "Sabão em barra",
+        },
+      ],
+      ...override,
+    });
+
+    await inMemoryDatabase.create(sale);
+
+    return sale;
+  }
+}

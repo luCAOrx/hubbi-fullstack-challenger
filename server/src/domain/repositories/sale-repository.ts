c0547588be356da7349c991820b51dca3a9aff02
luCAOrx <lucaorx@protonmail.com>
2025-01-29 08:12:37 -0300

    chore: Add findMany method

--- server/src/domain/repositories/sale-repository.ts
+++ server/src/domain/repositories/sale-repository.ts
@@ -3,4 +3,5 @@ import { Sale } from "@domain/entities/sale/sale";
 export abstract class SaleRepository {
   abstract create(sale: Sale): Promise<Sale>;
   abstract exists(name: string): Promise<boolean>;
+  abstract findMany(page: number): Promise<Sale[]>;
 }

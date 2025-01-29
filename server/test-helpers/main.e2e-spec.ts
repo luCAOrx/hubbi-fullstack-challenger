782bc862b346b36248a12609cdfd1c40c1621de4
luCAOrx <lucaorx@protonmail.com>
2025-01-29 10:33:42 -0300

    test(Helpers): Add message to information creation from 20 sales and add for loop to create 20 sales

--- server/test-helpers/main.e2e-spec.ts
+++ server/test-helpers/main.e2e-spec.ts
@@ -3,7 +3,10 @@ import { PrismaTestEnvironment } from "prisma/prismaTestEnvironment";
 
 import { app } from "@infra/http/app";
 import { createSaleControllerEndToEndTests } from "@infra/http/controllers/create-sale/create-sale-controller.e2e-spec";
+import { getSalesControllerEndToEndTests } from "@infra/http/controllers/get-sales/get-sales-controller-e2e-spec";
 import { pageNotFoundErrorEndToEndTests } from "@infra/http/errors/page-not-found/page-not-found-error.e2e-spec";
+
+import { MakeSaleFactory } from "./factories/make-sale-factory";
 
 const prismaTestEnvironment = new PrismaTestEnvironment();
 const server = app.listen(process.env.TEST_SERVER_PORT);
@@ -16,9 +19,22 @@ describe("End to end (E2E) tests", () => {
 
     await prismaTestEnvironment.setup();
     await new Promise((resolve) => server.once("listening", resolve));
+
+    process.stdout.write(`Registering 20 sales, wait...\n`);
+
+    for (let i = 0; i < 20; i++) {
+      const letter = String.fromCharCode(65 + i);
+
+      await new MakeSaleFactory().toHttp({
+        override: {
+          name: `Doces ${letter}`,
+        },
+      });
+    }
   });
 
   createSaleControllerEndToEndTests();
+  getSalesControllerEndToEndTests();
   pageNotFoundErrorEndToEndTests();
 
   after(async () => {

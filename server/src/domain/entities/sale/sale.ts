64c94752b1ac8b066f781aa21090b5a2b29853df
luCAOrx <lucaorx@protonmail.com>
2025-01-29 07:27:04 -0300

    chore: Add products and id properties, add validation to products property, remove private and public id atributes

--- server/src/domain/entities/sale/sale.ts
+++ server/src/domain/entities/sale/sale.ts
@@ -1,25 +1,22 @@
 import { randomUUID } from "node:crypto";
 import { NameValidation } from "src/core/logic/domain/validations/name-validation";
+import { ProductsValidation } from "src/core/logic/domain/validations/products-validation";
 
 export type Status = "Pendente" | "Finalizada";
 
 interface SaleProps {
   name: string;
   status: Status;
+  products: string;
 }
 
 export class Sale {
-  private readonly _private_id: number;
-  private readonly _public_id: string;
+  private readonly _id: string;
   private _props: SaleProps;
   private readonly _created_at: Date;
 
-  public get private_id(): number {
-    return this._private_id;
-  }
-
-  public get public_id(): string {
-    return this._public_id;
+  public get id(): string {
+    return this._id;
   }
 
   public get props(): SaleProps {
@@ -34,22 +31,13 @@ export class Sale {
     return this._created_at;
   }
 
-  private constructor(
-    { name, status }: SaleProps,
-    private_id?: number,
-    public_id?: string,
-  ) {
-    this._private_id = private_id ?? 0;
-    this._public_id = public_id ?? randomUUID();
-    this._props = { name, status };
+  private constructor({ name, products, status }: SaleProps, _id?: string) {
+    this._id = _id ?? randomUUID();
+    this._props = { name, status, products };
     this._created_at = new Date();
   }
 
-  static create(
-    { name, status }: SaleProps,
-    private_id?: number,
-    public_id?: string,
-  ): Sale {
+  static create({ name, products, status }: SaleProps, _id?: string): Sale {
     return new Sale(
       {
         name: NameValidation.valid({
@@ -59,9 +47,12 @@ export class Sale {
           lessThan: 150,
         }),
         status,
+        products: ProductsValidation.valid({
+          propertyName: "products",
+          propertyValue: products,
+        }),
       },
-      private_id,
-      public_id,
+      _id,
     );
   }
 }

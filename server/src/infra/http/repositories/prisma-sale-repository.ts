e559eb2e44485a8d2c2c1cfd5f05cd9dfb4b2632
luCAOrx <lucaorx@protonmail.com>
2025-01-29 06:52:11 -0300

    chore: Add sale repository to manage datas with prisma orm

---
+++ server/src/infra/http/repositories/prisma-sale-repository.ts
@@ -0,0 +1,54 @@
+import { randomUUID } from "node:crypto";
+
+import { Sale } from "@domain/entities/sale/sale";
+import { SaleRepository } from "@domain/repositories/sale-repository";
+
+import { prisma } from "../libs/prisma-client";
+import { SaleMapper } from "../mappers/sale-mapper";
+export class PrismaSaleRepository implements SaleRepository {
+  async create(sale: Sale): Promise<Sale> {
+    const { id, name, status, created_at, products } =
+      SaleMapper.toPersistence(sale);
+
+    const { createdSale, createdSaleProducts } = await prisma.$transaction(
+      async (trx) => {
+        const createdSale = await trx.sale.create({
+          data: {
+            id,
+            name,
+            status,
+            created_at,
+          },
+        });
+
+        const productIds = products
+          .split(",")
+          .map((product: string) => product.trim());
+
+        const createdSaleProducts = await Promise.all(
+          productIds.map((productId) =>
+            trx.saleProduct.create({
+              data: {
+                saleId: createdSale.id,
+                productId: productId,
+                id: randomUUID(),
+              },
+            }),
+          ),
+        );
+
+        return { createdSale, createdSaleProducts };
+      },
+    );
+
+    return SaleMapper.toDomain(createdSale, createdSaleProducts);
+  }
+
+  async exists(name: string): Promise<boolean> {
+    const saleAlreadyExists = await prisma.sale.findUnique({
+      where: { name },
+    });
+
+    return saleAlreadyExists !== null;
+  }
+}

1afe66a2787c128bbb3ba5c9b5094cd79cc1689d
luCAOrx <lucaorx@protonmail.com>
2025-01-29 06:57:22 -0300

    chore: Add route to api documentation, create sale and catch all in all routes to display HTTP status 404 if they don't exist

--- server/src/infra/http/app.ts
+++ server/src/infra/http/app.ts
@@ -1,10 +1,26 @@
+import "express-async-errors";
+
 import cors from "cors";
 import express from "express";
+import { serve, setup } from "swagger-ui-express";
+
+import { swaggerSpec } from "@doc/swagger";
+
+import { pageNotFoundError } from "./errors/page-not-found/page-not-found-error";
+import { createSaleRoute } from "./routes/create-sale.routes";
 
 export const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.get("/", (request, response) => {
-  response.send("Hello world");
-});
+app.use(
+  "/api-docs",
+  serve,
+  setup(swaggerSpec, {
+    customSiteTitle: "Desafio Full-Stack NodeJS & ReactJS",
+    swaggerUrl: String(process.env.DOCUMENTATION_SERVER_URL),
+  }),
+);
+
+app.use(createSaleRoute);
+app.all("*", pageNotFoundError);

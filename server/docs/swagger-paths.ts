5abefec3a51147189b5ad10db6f59edfa9994b62
luCAOrx <lucaorx@protonmail.com>
2025-01-29 10:29:28 -0300

    docs: Add route to get sales

--- server/docs/swagger-paths.ts
+++ server/docs/swagger-paths.ts
@@ -1,11 +1,16 @@
 import { type Paths } from "swagger-jsdoc";
 
 import { createSaleRequest } from "./src/infra/http/routes/create-sale/create-sale-request";
+import { getSalesRequest } from "./src/infra/http/routes/get-sales/get-sales-request";
 import { pageNotFoundRequest } from "./src/infra/http/routes/global-responses/page-not-found-request";
 
 export const swaggerPaths: Paths = {
   "/create-sale": {
     post: createSaleRequest,
+  },
+
+  "/get-sales": {
+    get: getSalesRequest,
   },
 
   "/fake-route": {

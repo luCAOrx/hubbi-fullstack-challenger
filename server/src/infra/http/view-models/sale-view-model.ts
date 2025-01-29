f4850f1634dcede7a76b94097bcdd014f0515f35
luCAOrx <lucaorx@protonmail.com>
2025-01-29 06:54:31 -0300

    chore: Add sale view model to manage how data are displayed

---
+++ server/src/infra/http/view-models/sale-view-model.ts
@@ -0,0 +1,25 @@
+import { Sale, Status } from "@domain/entities/sale/sale";
+
+export interface ToHttpResponse {
+  id: string;
+  name: string;
+  status: Status;
+  products: string;
+  created_at: Date;
+}
+
+export class SaleViewModel {
+  static toHttp({
+    id,
+    props: { name, status, products },
+    created_at,
+  }: Sale): ToHttpResponse {
+    return {
+      id,
+      name,
+      status,
+      products,
+      created_at,
+    };
+  }
+}

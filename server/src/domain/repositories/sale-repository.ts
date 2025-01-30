219201771ec0c4ec13944aa9d30712ba9866178a
luCAOrx <lucaorx@protonmail.com>
2025-01-30 05:27:09 -0300

    chore: Remove update method

--- server/src/domain/repositories/sale-repository.ts
+++ server/src/domain/repositories/sale-repository.ts
@@ -3,7 +3,6 @@ import { Sale } from "@domain/entities/sale/sale";
 export abstract class SaleRepository {
   abstract create(sale: Sale): Promise<Sale>;
   abstract exists(name: string): Promise<boolean>;
-  abstract update(sale: Sale): Promise<Sale>;
   abstract findById(id: string): Promise<Sale | null>;
   abstract findMany(page: number): Promise<Sale[]>;
 }

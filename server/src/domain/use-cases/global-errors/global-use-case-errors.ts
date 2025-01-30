5731f18e77824d474dd7b4da8fd6adf225354e77
luCAOrx <lucaorx@protonmail.com>
2025-01-29 23:20:42 -0300

    chore: Remove global use case errors

--- server/src/domain/use-cases/global-errors/global-use-case-errors.ts
+++
@@ -1,9 +0,0 @@
-export namespace GlobalUseCaseErrors {
-  export class SaleAlreadyExistsError extends Error {
-    constructor() {
-      super("ASaleWithThatNameAlreadyExists");
-
-      this.message = "Uma venda com esse nome já existe";
-    }
-  }
-}

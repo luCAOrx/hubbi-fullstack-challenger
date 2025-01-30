04fdb99c9afc560289ea8d6aacdf0e38f0efd77d
luCAOrx <lucaorx@protonmail.com>
2025-01-30 05:24:36 -0300

    test(Helpers): Add setup and teardown methods to work dynamically

--- server/prisma/prismaTestEnvironment.ts
+++ server/prisma/prismaTestEnvironment.ts
@@ -1,16 +1,37 @@
 import { execSync } from "node:child_process";
-
-import { prisma } from "@infra/http/libs/prisma-client";
+import { randomUUID } from "node:crypto";
+import { Client } from "pg";
 
 export class PrismaTestEnvironment {
-  async setup(): Promise<void> {
+  private schema: string;
+  private connectionString: string;
+
+  constructor() {
+    const dbUser = process.env.DATABASE_USER;
+    const dbPass = process.env.DATABASE_PASSWORD;
+    const dbHost = process.env.DATABASE_HOST;
+    const dbPort = process.env.DATABASE_PORT;
+    const dbName = process.env.DATABASE_NAME;
+
+    this.schema = `test_${randomUUID()}`;
+    this.connectionString = `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?schema=${this.schema}`;
+  }
+
+  async setup() {
+    process.env.DATABASE_URL = this.connectionString;
+    process.env.DATABASE_URL = this.connectionString;
+
     execSync("npm run prisma:migrate");
     execSync("npm run prisma:seed:test");
   }
 
-  async teardown(): Promise<void> {
-    await prisma.sale.deleteMany();
-    await prisma.product.deleteMany();
-    await prisma.saleProduct.deleteMany();
+  async teardown() {
+    const client = new Client({
+      connectionString: this.connectionString,
+    });
+
+    await client.connect();
+    await client.query(`DROP SCHEMA IF EXISTS "${this.schema}" CASCADE`);
+    await client.end();
   }
 }

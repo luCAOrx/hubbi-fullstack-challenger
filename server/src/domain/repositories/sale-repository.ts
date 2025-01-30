04ac1da964b6d532810ebea60405f8af3621e5dd
luCAOrx <lucaorx@protonmail.com>
2025-01-29 23:13:22 -0300

    chore: Add update and findById methods

--- server/src/domain/repositories/sale-repository.ts
+++ server/src/domain/repositories/sale-repository.ts
@@ -3,5 +3,7 @@ import { Sale } from "@domain/entities/sale/sale";
 export abstract class SaleRepository {
   abstract create(sale: Sale): Promise<Sale>;
   abstract exists(name: string): Promise<boolean>;
+  abstract update(sale: Sale): Promise<Sale>;
+  abstract findById(id: string): Promise<Sale | null>;
   abstract findMany(page: number): Promise<Sale[]>;
 }

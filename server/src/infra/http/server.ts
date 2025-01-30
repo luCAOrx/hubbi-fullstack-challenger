3c8dea81383d2e7ad81537cdd255c18bbc94aa10
luCAOrx <lucaorx@protonmail.com>
2025-01-30 10:23:24 -0300

    chore: Remove console.log

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,7 +1,3 @@
 import { app } from "./app";
 
-app.listen(process.env.PORT ?? process.env.SERVER_PORT, () => {
-  console.log(
-    `Server running in port: ${process.env.PORT ?? process.env.SERVER_PORT}`,
-  );
-});
+app.listen(process.env.PORT ?? process.env.SERVER_PORT);

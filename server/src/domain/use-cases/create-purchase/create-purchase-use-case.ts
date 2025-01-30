7b8bc51a432c945bbe0809628774ec4d694fdbbb
luCAOrx <lucaorx@protonmail.com>
2025-01-29 23:15:42 -0300

    feat: Add create purchase use case

---
+++ server/src/domain/use-cases/create-purchase/create-purchase-use-case.ts
@@ -0,0 +1,44 @@
+import { Purchase } from "@domain/entities/purchase/purchase";
+import { PurchaseRepository } from "@domain/repositories/purchase-repository";
+import { SaleRepository } from "@domain/repositories/sale-repository";
+
+import { BaseUseCase } from "../base-use-case";
+import { CreatePurchaseUseCaseErrors } from "./errors/sale-not-found-error";
+
+interface CreatePurchaseRequest {
+  saleId: string;
+  products: string;
+}
+
+interface CreatePurchaseResponse {
+  purchase: Purchase;
+}
+
+export class CreatePurchaseUseCase
+  implements BaseUseCase<CreatePurchaseRequest, CreatePurchaseResponse>
+{
+  constructor(
+    private readonly purchaseRepository: PurchaseRepository,
+    private readonly saleRepository: SaleRepository,
+  ) {}
+
+  async execute({
+    saleId,
+    products,
+  }: CreatePurchaseRequest): Promise<CreatePurchaseResponse> {
+    const saleOrNull = await this.saleRepository.findById(saleId);
+
+    if (saleOrNull === null) {
+      throw new CreatePurchaseUseCaseErrors.SaleNotFoundError();
+    }
+
+    const purchase = Purchase.create({ saleId, products });
+
+    saleOrNull.props.status = "Finalizada";
+
+    await this.purchaseRepository.create(purchase);
+    await this.saleRepository.update(saleOrNull);
+
+    return { purchase };
+  }
+}

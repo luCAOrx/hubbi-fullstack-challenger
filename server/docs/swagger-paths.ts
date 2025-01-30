62f636bfd38f35c1ac5d975f588cd2ffb56c931d
luCAOrx <lucaorx@protonmail.com>
2025-01-30 05:23:12 -0300

    docs: Add route to create-purchase

--- server/docs/swagger-paths.ts
+++ server/docs/swagger-paths.ts
@@ -1,5 +1,6 @@
 import { type Paths } from "swagger-jsdoc";
 
+import { createPurchaseRequest } from "./src/infra/http/routes/create-purchase/create-purchase-request";
 import { createSaleRequest } from "./src/infra/http/routes/create-sale/create-sale-request";
 import { getSalesRequest } from "./src/infra/http/routes/get-sales/get-sales-request";
 import { pageNotFoundRequest } from "./src/infra/http/routes/global-responses/page-not-found-request";
@@ -11,6 +12,10 @@ export const swaggerPaths: Paths = {
 
   "/get-sales": {
     get: getSalesRequest,
+  },
+
+  "/create-purchase/{saleId}": {
+    post: createPurchaseRequest,
   },
 
   "/fake-route": {

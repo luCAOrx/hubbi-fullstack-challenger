4674de8c1364f30225ef9dd8858cdc8213086f26
luCAOrx <lucaorx@protonmail.com>
2025-02-01 17:30:04 -0300

    feat: Add get products controller

---
+++ server/src/infra/http/controllers/get-products/get-products-controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+
+import { prisma } from "@infra/http/libs/prisma-client";
+
+import { BaseController } from "../base-controller";
+
+export class GetProductsController extends BaseController {
+  protected async executeImplementation(
+    request: Request,
+    response: Response,
+  ): Promise<any> {
+    await prisma.product
+      .findMany({})
+      .then((productOrProducts) => {
+        return this.ok({
+          response,
+          message: { productOrProducts },
+        });
+      })
+      .catch((error: Error) => {
+        return this.clientError({
+          response,
+          message: error.message,
+        });
+      });
+  }
+}

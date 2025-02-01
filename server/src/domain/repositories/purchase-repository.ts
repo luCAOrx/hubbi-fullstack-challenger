71a84e8642059974c5bba64b41031d09c1c92084
luCAOrx <lucaorx@protonmail.com>
2025-02-01 11:10:11 -0300

    chore: Change name from method create, add perPage in param from findMany method, add method to get purchases count

--- server/src/domain/repositories/purchase-repository.ts
+++ server/src/domain/repositories/purchase-repository.ts
@@ -1,6 +1,9 @@
 import { Purchase } from "@domain/entities/purchase/purchase";
 
 export abstract class PurchaseRepository {
-  abstract create(purchase: Purchase): Promise<Purchase>;
-  abstract findMany(page: number): Promise<Purchase[]>;
+  abstract createPurchaseWithTotalPurchases(
+    purchase: Purchase,
+  ): Promise<Purchase>;
+  abstract findMany(page: number, perPage: number): Promise<Purchase[]>;
+  abstract getTotalPurchasesCount(): Promise<number>;
 }

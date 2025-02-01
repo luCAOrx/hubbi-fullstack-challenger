ae8c4bdd4b1f6dd8e0fec60e4e1768efd790f1c0
luCAOrx <lucaorx@protonmail.com>
2025-02-01 11:20:39 -0300

    test(Helpers): Add Counter-based Denormalization strategy

--- server/test-helpers/in-memory-database/in-memory-purchase-database.ts
+++ server/test-helpers/in-memory-database/in-memory-purchase-database.ts
@@ -1,10 +1,19 @@
 import { Purchase } from "@domain/entities/purchase/purchase";
 import { PurchaseRepository } from "@domain/repositories/purchase-repository";
 
+interface PurchaseCounter {
+  totalPurchases: number;
+}
+
 export class InMemoryPurchaseDatabase implements PurchaseRepository {
   public purchases: Purchase[] = [];
+  public purchaseCounter: PurchaseCounter = { totalPurchases: 0 };
 
-  async create(purchase: Purchase): Promise<Purchase> {
+  async createPurchaseWithTotalPurchases(
+    purchase: Purchase,
+  ): Promise<Purchase> {
+    this.purchaseCounter.totalPurchases++;
+
     this.purchases.push(purchase);
 
     return purchase;
@@ -14,5 +23,9 @@ export class InMemoryPurchaseDatabase implements PurchaseRepository {
     const purchaseOrPurchases = this.purchases.map((purchases) => purchases);
 
     return purchaseOrPurchases.slice((page - 1) * 10, page * 10);
+  }
+
+  async getTotalPurchasesCount(): Promise<number> {
+    return this.purchaseCounter.totalPurchases ?? 0;
   }
 }

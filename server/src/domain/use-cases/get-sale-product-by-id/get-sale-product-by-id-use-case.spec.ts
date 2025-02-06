f97629a3ca57c5f0b234577bb2dce98368c5981c
luCAOrx <lucaorx@protonmail.com>
2025-02-06 02:25:00 -0300

    style: Add suffix -use-case to filename

--- server/src/domain/use-cases/get-sale-product-by-id/get-sale-product-by-id-use-case.spec.ts
+++ server/src/domain/use-cases/get-sale-product-by-id/get-sale-product-by-id-use-case.spec.ts
@@ -5,7 +5,7 @@ import { MakeSaleFactory } from "@test-helpers/factories/make-sale-factory";
 import { InMemorySaleDatabase } from "@test-helpers/in-memory-database/in-memory-sale-database";
 
 import { GlobalUseCaseErrors } from "../global-errors/global-use-case-errors";
-import { GetSaleProductByIdUseCase } from "./get-sale-product-by-id";
+import { GetSaleProductByIdUseCase } from "./get-sale-product-by-id-use-case";
 
 describe("Get sale product by id use case", () => {
   const inMemorySaleDatabase = new InMemorySaleDatabase();
@@ -24,10 +24,10 @@ describe("Get sale product by id use case", () => {
       .then(async (sale) => {
         await getSaleProductById
           .execute({ saleId: sale.id })
-          .then(({ saleProducts }) => {
-            deepStrictEqual(saleProducts.products?.length, 3);
+          .then(({ saleProduct }) => {
+            deepStrictEqual(saleProduct.products?.length, 3);
 
-            const products = saleProducts.products.map((product) => product);
+            const products = saleProduct.products.map((product) => product);
 
             deepStrictEqual(
               products[0].props.name,

976b3d81cdff4b8625152e1c84693ffa4444a148
luCAOrx <lucaorx@protonmail.com>
2025-02-06 02:21:01 -0300

    feat: Add get sale product by id use case

---
+++ server/src/domain/use-cases/get-sale-product-by-id/get-sale-product-by-id.ts
@@ -0,0 +1,31 @@
+import { Sale } from "@domain/entities/sale/sale";
+import { SaleRepository } from "@domain/repositories/sale-repository";
+
+import { BaseUseCase } from "../base-use-case";
+import { GlobalUseCaseErrors } from "../global-errors/global-use-case-errors";
+
+interface GetSaleProductByIdRequest {
+  saleId: string;
+}
+
+export interface GetSaleProductByIdResponse {
+  saleProduct: Sale;
+}
+
+export class GetSaleProductByIdUseCase
+  implements BaseUseCase<GetSaleProductByIdRequest, GetSaleProductByIdResponse>
+{
+  constructor(private readonly saleRepository: SaleRepository) {}
+
+  async execute({
+    saleId,
+  }: GetSaleProductByIdRequest): Promise<GetSaleProductByIdResponse> {
+    const saleOrNull = await this.saleRepository.findSaleProductById(saleId);
+
+    if (saleOrNull === null) {
+      throw new GlobalUseCaseErrors.SaleNotFoundError();
+    }
+
+    return { saleProduct: saleOrNull };
+  }
+}

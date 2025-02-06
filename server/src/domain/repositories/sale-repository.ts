c142279ea754aac32b4081a434014754cb712346
luCAOrx <lucaorx@protonmail.com>
2025-02-06 02:32:08 -0300

    chore: Add method to find sale product by id

--- server/src/domain/repositories/sale-repository.ts
+++ server/src/domain/repositories/sale-repository.ts
@@ -4,6 +4,7 @@ export abstract class SaleRepository {
   abstract createSaleWithTotalSales(sale: Sale): Promise<Sale>;
   abstract exists(name: string): Promise<boolean>;
   abstract findById(id: string): Promise<Sale | null>;
+  abstract findSaleProductById(saleId: string): Promise<Sale | null>;
   abstract findMany(page: number, perPage: number): Promise<Sale[]>;
   abstract getTotalSalesCount(): Promise<number>;
 }

11531062e9bade2c1abd4491b25985ed2aa7109e
luCAOrx <lucaorx@protonmail.com>
2025-02-06 04:29:41 -0300

    test(Helpers): Add find sale product by id method and product creation when create a sale

--- server/test-helpers/in-memory-database/in-memory-sale-database.ts
+++ server/test-helpers/in-memory-database/in-memory-sale-database.ts
@@ -1,3 +1,4 @@
+import { Product } from "@domain/entities/product/product";
 import { Sale } from "@domain/entities/sale/sale";
 import { SaleRepository } from "@domain/repositories/sale-repository";
 
@@ -7,11 +8,25 @@ interface SaleCounter {
 
 export class InMemorySaleDatabase implements SaleRepository {
   public sales: Sale[] = [];
+  public products: Product[] = [];
   public saleCounter: SaleCounter = { totalSales: 0 };
 
   async createSaleWithTotalSales(sale: Sale): Promise<Sale> {
     this.saleCounter.totalSales++;
 
+    for (let i = 0; i < 3; i++) {
+      const product = Product.create({ name: `Cachorro quente ${i}` }, {});
+      this.products.push(product);
+    }
+
+    sale = Sale.create(
+      {
+        name: sale.props.name,
+        products: sale.props.products,
+        status: sale.props.status,
+      },
+      { _products: this.products, _id: sale.id },
+    );
     this.sales.push(sale);
 
     return sale;
@@ -27,6 +42,23 @@ export class InMemorySaleDatabase implements SaleRepository {
     if (sale == null) return null;
 
     return sale;
+  }
+
+  async findSaleProductById(saleId: string): Promise<Sale | null> {
+    const saleOrNull = this.sales.find((sale) => sale.id === saleId);
+
+    if (saleOrNull == null) return null;
+
+    const products = saleOrNull.products?.map((product) => product);
+
+    return Sale.create(
+      {
+        products: saleOrNull.props.products,
+        name: saleOrNull.props.name,
+        status: saleOrNull.props.status,
+      },
+      { _products: products! },
+    );
   }
 
   async findMany(page: number, perPage: number): Promise<Sale[]> {

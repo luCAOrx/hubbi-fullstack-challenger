b9ee4a4f4f6b58b9e736d3792307db418dfa5374
luCAOrx <lucaorx@protonmail.com>
2025-02-06 04:23:44 -0300

    chore: Change properties to display in return

--- server/src/infra/http/view-models/get-sales-view-model.ts
+++ server/src/infra/http/view-models/get-sales-view-model.ts
@@ -1,12 +1,17 @@
-import { Sale } from "@domain/entities/sale/sale";
-import { GeSalesResponse } from "@domain/use-cases/get-sales/get-sales-use-case";
+import { Status } from "@domain/entities/sale/sale";
+import { GetSalesResponse } from "@domain/use-cases/get-sales/get-sales-use-case";
 
-export interface ToHttpResponse {
+export interface GetSalesToHttpResponse {
   page: number;
   perPage: number;
   pages: number;
   totalSales: number;
-  data: Sale[];
+  data: {
+    id: string;
+    name: string;
+    status?: Status;
+    created_at: Date;
+  }[];
 }
 
 export class GetSalesViewModel {
@@ -16,13 +21,22 @@ export class GetSalesViewModel {
     pages,
     totalSales,
     data,
-  }: GeSalesResponse): ToHttpResponse {
+  }: GetSalesResponse): GetSalesToHttpResponse {
+    const sales = data.map((sale) => {
+      return {
+        id: sale.id,
+        name: sale.props.name,
+        status: sale.props.status,
+        created_at: sale.created_at,
+      };
+    });
+
     return {
       page,
       perPage,
       pages,
       totalSales,
-      data,
+      data: sales,
     };
   }
 }

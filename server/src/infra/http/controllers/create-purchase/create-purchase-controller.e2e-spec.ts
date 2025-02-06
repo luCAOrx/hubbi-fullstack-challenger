d47e0b4c3e188c07dc57b01e0314bbd12a4365fa
luCAOrx <lucaorx@protonmail.com>
2025-02-06 03:28:25 -0300

    test(E2E): Removed delay bug that delayed the test for a long time and add refactoring test to turn more readble

--- server/src/infra/http/controllers/create-purchase/create-purchase-controller.e2e-spec.ts
+++ server/src/infra/http/controllers/create-purchase/create-purchase-controller.e2e-spec.ts
@@ -1,57 +1,44 @@
 import { deepStrictEqual } from "node:assert";
 import { describe, it } from "node:test";
 
-import { prisma } from "@infra/http/libs/prisma-client";
+import { GetSalesToHttpResponse } from "@infra/http/view-models/get-sales-view-model";
+import { CreatePurchaseToHttpResponse } from "@infra/http/view-models/purchase-view-model";
 import { MakePurchaseFactory } from "@test-helpers/factories/make-purchase-factory";
 import { MakeRequestFactory } from "@test-helpers/factories/make-request-factory";
 import { MakeSaleFactory } from "@test-helpers/factories/make-sale-factory";
+import { saleId } from "@test-helpers/main.e2e-spec";
 
 export function createPurchaseControllerEndToEndTests(): void {
-  describe("Create purchase controller", async () => {
+  describe("Create purchase controller", () => {
     const headers = new Headers();
     headers.append("Content-Type", "application/json");
 
-    const saleId = await new MakeSaleFactory()
-      .toHttp({
-        override: {
-          name: "Doces de festa",
-        },
-      })
-      .then(async (response) => {
-        const {
-          sale: { id },
-        }: any = await response.json();
+    it("should be able create new purchase", async () => {
+      const status = await MakeRequestFactory.execute({
+        url: `${String(process.env.TEST_SERVER_URL)}/get-sales?page=3`,
+        method: "GET",
+        headers,
+      }).then(async (response) => {
+        const { data }: GetSalesToHttpResponse =
+          (await response.json()) as GetSalesToHttpResponse;
 
-        return id;
+        return data[0].status;
       });
 
-    it("should be able create new purchase", async () => {
       await new MakePurchaseFactory()
-        .toHttp({ saleId })
+        .toHttp({ saleId: saleId[0] })
         .then(async (response) => {
-          const purchaseResponse: any = await response
-            .json()
-            .then((response) => response);
-
-          const saleOrNull = await prisma.sale.findUnique({
-            where: {
-              id: purchaseResponse.purchase.id,
-            },
-            select: { status: true },
-          });
-
-          if (saleOrNull === null) return null;
+          const purchase: CreatePurchaseToHttpResponse =
+            (await response.json()) as CreatePurchaseToHttpResponse;
 
           deepStrictEqual(response.status, 201);
-          deepStrictEqual(saleOrNull.status, "Finalizada");
-          deepStrictEqual(purchaseResponse, {
-            purchase: {
-              id: purchaseResponse.purchase.id,
-              saleId: purchaseResponse.purchase.saleId,
-              products:
-                "d2ef3c85-a5ed-4fcb-bc50-22e04e3dd43f,1831c265-4d88-4184-bd8b-82b87c6458f7,4ceeeda9-e10e-4453-9874-e70fb27bb1b8",
-              created_at: purchaseResponse.purchase.created_at,
-            },
+          deepStrictEqual(status, "Finalizada");
+          deepStrictEqual(purchase, {
+            id: purchase.id,
+            saleId: purchase.saleId,
+            products:
+              "d2ef3c85-a5ed-4fcb-bc50-22e04e3dd43f,1831c265-4d88-4184-bd8b-82b87c6458f7,4ceeeda9-e10e-4453-9874-e70fb27bb1b8",
+            created_at: purchase.created_at,
           });
         });
     });
@@ -74,6 +61,18 @@ export function createPurchaseControllerEndToEndTests(): void {
     });
 
     it("should not be able to create new purchase without properties of request body", async () => {
+      const saleId = await new MakeSaleFactory()
+        .toHttp({
+          override: {
+            name: "Create Purchase Without Properties",
+          },
+        })
+        .then(async (response) => {
+          const data: any = await response.json();
+
+          return data.sale.id;
+        });
+
       await MakeRequestFactory.execute({
         url: `${String(process.env.TEST_SERVER_URL)}/create-purchase/${saleId}`,
         method: "POST",
@@ -93,6 +92,20 @@ export function createPurchaseControllerEndToEndTests(): void {
     });
 
     it("should not be able to create new purchase if property of request body not same products", async () => {
+      const saleId = await new MakeSaleFactory()
+        .toHttp({
+          override: {
+            name: "Get sale product by id test C",
+          },
+        })
+        .then(async (response) => {
+          const {
+            sale: { id },
+          }: any = await response.json();
+
+          return id;
+        });
+
       await MakeRequestFactory.execute({
         url: `${String(process.env.TEST_SERVER_URL)}/create-purchase/${saleId}`,
         method: "POST",
@@ -114,6 +127,18 @@ export function createPurchaseControllerEndToEndTests(): void {
     });
 
     it("should not be able to create new purchase with field products empty", async () => {
+      const saleId = await new MakeSaleFactory()
+        .toHttp({
+          override: {
+            name: "Get sale product by id test D",
+          },
+        })
+        .then(async (response) => {
+          const data: any = await response.json();
+
+          return data.sale.id;
+        });
+
       await new MakePurchaseFactory()
         .toHttp({ saleId, override: { products: "" } })
         .then(async (response) => {

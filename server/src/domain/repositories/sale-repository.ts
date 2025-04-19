eaf828e89e639d9b0a1628d5f21810eaaac262bd
luCAOrx <lucaorx@protonmail.com>
2025-04-19 06:31:24 -0300

    chore: Change method name to create sale, add sale and saleProducts parameters to create sale method, change return from find sale product by id method

--- server/src/domain/repositories/sale-repository.ts
+++ server/src/domain/repositories/sale-repository.ts
@@ -1,10 +1,14 @@
+import { SaleProduct } from "@domain/entities/sale-product/sale-product";
 import { Sale } from "@domain/entities/sale/sale";
 
 export abstract class SaleRepository {
-  abstract createSaleWithTotalSales(sale: Sale): Promise<Sale>;
+  abstract transactionCreateSaleWithSaleProductAndSaleCounter(
+    sale: Sale,
+    saleProduct: SaleProduct[],
+  ): Promise<Sale>;
   abstract exists(name: string): Promise<boolean>;
   abstract findById(id: string): Promise<Sale | null>;
-  abstract findSaleProductById(saleId: string): Promise<Sale | null>;
+  abstract findSaleProductById(saleId: string): Promise<SaleProduct[]>;
   abstract findMany(page: number, perPage: number): Promise<Sale[]>;
   abstract getTotalSalesCount(): Promise<number>;
 }

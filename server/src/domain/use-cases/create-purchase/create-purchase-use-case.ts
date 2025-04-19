37f84ba171370285c84a4f69e5159d0ed4a33740
luCAOrx <lucaorx@protonmail.com>
2025-04-19 18:05:58 -0300

    chore: Remove products property and add saleProductId, add verify to be able create purchase only if products are part of the sale supplied, add create from PurchaseSaleProduct class, add PurchaseSaleProduct class to Purchase property, add method to create purchase and purchaseSaleProduct together

--- server/src/domain/use-cases/create-purchase/create-purchase-use-case.ts
+++ server/src/domain/use-cases/create-purchase/create-purchase-use-case.ts
@@ -1,13 +1,15 @@
+import { PurchaseSaleProduct } from "@domain/entities/purchase-sale-product/purchase-sale-product";
 import { Purchase } from "@domain/entities/purchase/purchase";
 import { PurchaseRepository } from "@domain/repositories/purchase-repository";
 import { SaleRepository } from "@domain/repositories/sale-repository";
 
 import { BaseUseCase } from "../base-use-case";
 import { GlobalUseCaseErrors } from "../global-errors/global-use-case-errors";
+import { CreatePurchaseUseCaseErrors } from "./errors/these-products-are-not-part-of-this-sale-error";
 
 interface CreatePurchaseRequest {
   saleId: string;
-  products: string;
+  saleProductId: string;
 }
 
 interface CreatePurchaseResponse {
@@ -24,7 +26,7 @@ export class CreatePurchaseUseCase
 
   async execute({
     saleId,
-    products,
+    saleProductId,
   }: CreatePurchaseRequest): Promise<CreatePurchaseResponse> {
     const saleOrNull = await this.saleRepository.findById(saleId);
 
@@ -32,9 +34,56 @@ export class CreatePurchaseUseCase
       throw new GlobalUseCaseErrors.SaleNotFoundError();
     }
 
-    const purchase = Purchase.create({ saleId, products });
+    const saleProductOrSaleProducts =
+      await this.saleRepository.findSaleProductById(saleOrNull.id);
 
-    await this.purchaseRepository.createPurchaseWithTotalPurchases(purchase);
+    const saleProductIds = saleProductId
+      .split(",")
+      .map((saleProductId) => saleProductId);
+
+    const isProductArePartFromSaleProduct = saleProductIds.every(
+      (saleProductId) =>
+        saleProductOrSaleProducts
+          .map((saleProduct) => saleProduct.id)
+          .includes(saleProductId),
+    );
+
+    if (!isProductArePartFromSaleProduct) {
+      throw new CreatePurchaseUseCaseErrors.TheseProductsAreNotPartOfThisSaleError();
+    }
+
+    let purchase = Purchase.create(
+      { saleId },
+      {
+        _sale: saleOrNull,
+      },
+    );
+
+    const purchaseSaleProducts = saleProductIds.map((saleProductId) => {
+      const purchaseSaleProduct = PurchaseSaleProduct.create(
+        {
+          saleProductId: saleProductId,
+          purchaseId: purchase.id,
+        },
+        { _purchase: purchase },
+      );
+
+      return purchaseSaleProduct;
+    });
+
+    purchase = Purchase.create(
+      { saleId },
+      {
+        _sale: saleOrNull,
+        _purchaseSaleProducts: purchaseSaleProducts,
+        _id: purchase.id,
+      },
+    );
+
+    await this.purchaseRepository.transactionCreatePurchaseWithPurchaseSaleProductAndPurchaseCounter(
+      purchase,
+      purchaseSaleProducts,
+    );
 
     return { purchase };
   }

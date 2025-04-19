4c428a9aa1ed629638cc7912dd555c7883549b59
luCAOrx <lucaorx@protonmail.com>
2025-04-19 06:27:55 -0300

    chore: Change method name to create purchase, add purchaseSaleProducts to parameter from create purchase method, add method to find purchase sale product by purchase id

--- server/src/domain/repositories/purchase-repository.ts
+++ server/src/domain/repositories/purchase-repository.ts
@@ -1,9 +1,14 @@
+import { PurchaseSaleProduct } from "@domain/entities/purchase-sale-product/purchase-sale-product";
 import { Purchase } from "@domain/entities/purchase/purchase";
 
 export abstract class PurchaseRepository {
-  abstract createPurchaseWithTotalPurchases(
+  abstract transactionCreatePurchaseWithPurchaseSaleProductAndPurchaseCounter(
     purchase: Purchase,
+    purchasSaleProducts: PurchaseSaleProduct[],
   ): Promise<Purchase>;
   abstract findMany(page: number, perPage: number): Promise<Purchase[]>;
+  abstract findPurchaseSaleProductByPurchaseId(
+    purchaseId: string,
+  ): Promise<Purchase | null>;
   abstract getTotalPurchasesCount(): Promise<number>;
 }

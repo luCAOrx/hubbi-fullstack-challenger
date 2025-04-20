5dd9d58850b1c2cfaed57fdc297171d008d300e8
luCAOrx <lucaorx@protonmail.com>
2025-04-20 04:04:47 -0300

    test(Unit): Remove inMemorySaleDatabase instance, add saleIds variable, remove MakeSaleFactory instance, add inMemoryPurchaseDatabase instance ,change property name to saleName, remove comparisons from lenght from sales array inMemoryDatabase, change comparisons from products to saleId

--- server/src/domain/use-cases/get-purchases/get-purchases-use-case.spec.ts
+++ server/src/domain/use-cases/get-purchases/get-purchases-use-case.spec.ts
@@ -2,32 +2,29 @@ import { deepStrictEqual, notDeepStrictEqual } from "node:assert";
 import { describe, it, before } from "node:test";
 
 import { MakePurchaseFactory } from "@test-helpers/factories/make-purchase-factory";
-import { MakeSaleFactory } from "@test-helpers/factories/make-sale-factory";
 import { InMemoryPurchaseDatabase } from "@test-helpers/in-memory-database/in-memory-purchase-database";
-import { InMemorySaleDatabase } from "@test-helpers/in-memory-database/in-memory-sale-database";
 
 import { GetPurchasesUseCase } from "./get-purchases-use-case";
 
 describe("Get purchases use case", () => {
   const inMemoryPurchaseDatabase = new InMemoryPurchaseDatabase();
-  const inMemorySaleDatabase = new InMemorySaleDatabase();
   const getPurchasesUseCase = new GetPurchasesUseCase(inMemoryPurchaseDatabase);
+  const saleIds: string[] = [];
 
   before(async () => {
     for (let i = 0; i < 20; i++) {
       const letter = String.fromCharCode(65 + i);
 
-      await new MakeSaleFactory().toDomain({
-        inMemoryDatabase: inMemorySaleDatabase,
-        override: {
-          name: `Doces ${letter}`,
-        },
-      });
-
-      await new MakePurchaseFactory().toDomain({
-        inMemoryDatabaseToPurchase: inMemoryPurchaseDatabase,
-        inMemoryDatabaseToSale: inMemorySaleDatabase,
-      });
+      await new MakePurchaseFactory()
+        .toDomain({
+          inMemoryDatabase: inMemoryPurchaseDatabase,
+          override: {
+            saleName: `Doces ${letter}`,
+          },
+        })
+        .then(({ sale }) => {
+          saleIds.push(sale.id);
+        });
     }
   });
 
@@ -40,18 +37,8 @@ describe("Get purchases use case", () => {
         deepStrictEqual(pages, 2);
         deepStrictEqual(perPage, 10);
         deepStrictEqual(totalPurchases, 20);
-        deepStrictEqual(inMemorySaleDatabase.sales.length, 40);
         deepStrictEqual(inMemoryPurchaseDatabase.purchases.length, 20);
-        deepStrictEqual(data[0].props.products, "1,2,3");
-        deepStrictEqual(data[1].props.products, "1,2,3");
-        deepStrictEqual(data[2].props.products, "1,2,3");
-        deepStrictEqual(data[3].props.products, "1,2,3");
-        deepStrictEqual(data[4].props.products, "1,2,3");
-        deepStrictEqual(data[5].props.products, "1,2,3");
-        deepStrictEqual(data[6].props.products, "1,2,3");
-        deepStrictEqual(data[7].props.products, "1,2,3");
-        deepStrictEqual(data[8].props.products, "1,2,3");
-        deepStrictEqual(data[9].props.products, "1,2,3");
+        deepStrictEqual(data[0].props.saleId, saleIds[0]);
       });
   });
 
@@ -64,18 +51,8 @@ describe("Get purchases use case", () => {
         deepStrictEqual(pages, 2);
         deepStrictEqual(perPage, 10);
         deepStrictEqual(totalPurchases, 20);
-        deepStrictEqual(inMemorySaleDatabase.sales.length, 40);
         deepStrictEqual(inMemoryPurchaseDatabase.purchases.length, 20);
-        deepStrictEqual(data[0].props.products, "1,2,3");
-        deepStrictEqual(data[1].props.products, "1,2,3");
-        deepStrictEqual(data[2].props.products, "1,2,3");
-        deepStrictEqual(data[3].props.products, "1,2,3");
-        deepStrictEqual(data[4].props.products, "1,2,3");
-        deepStrictEqual(data[5].props.products, "1,2,3");
-        deepStrictEqual(data[6].props.products, "1,2,3");
-        deepStrictEqual(data[7].props.products, "1,2,3");
-        deepStrictEqual(data[8].props.products, "1,2,3");
-        deepStrictEqual(data[9].props.products, "1,2,3");
+        deepStrictEqual(data[0].props.saleId, saleIds[0]);
       });
   });
 });

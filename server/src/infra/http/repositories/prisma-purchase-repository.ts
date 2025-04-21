07d3e3be38c6b28f0054a638370a1ee16ceb7441
luCAOrx <lucaorx@protonmail.com>
2025-04-21 05:53:06 -0300

    chore: Change create purchase method name. Add purchaseSaleProducts to params from create purchase method. Remove products from desruption from PurchaseMapper. Add variable to PurchaseSaleProduct.toPersistence method. Remove createdPurchaseProducts from transaction return. Add transaction to create purchaseSaleProducts. Remove rawPurchaseProducts from params from PurchaseMapper.toDomain method. Change rawSales param from PurchaseMapper.toDomain method to rawPrismaSale. Add method to find purchase sale product by purchase id

--- server/src/infra/http/repositories/prisma-purchase-repository.ts
+++ server/src/infra/http/repositories/prisma-purchase-repository.ts
@@ -1,91 +1,93 @@
-import { randomUUID } from "node:crypto";
-
+import { PurchaseSaleProduct } from "@domain/entities/purchase-sale-product/purchase-sale-product";
 import { Purchase } from "@domain/entities/purchase/purchase";
 import { PurchaseRepository } from "@domain/repositories/purchase-repository";
 
 import { prisma } from "../libs/prisma-client";
 import { PurchaseMapper } from "../mappers/purchase-mapper";
+import { PurchaseSaleProductMapper } from "../mappers/purchase-sale-product-mapper";
 
 export class PrismaPurchaseRepository implements PurchaseRepository {
-  async createPurchaseWithTotalPurchases(
+  async transactionCreatePurchaseWithPurchaseSaleProductAndPurchaseCounter(
     purchase: Purchase,
+    purchasSaleProducts: PurchaseSaleProduct[],
   ): Promise<Purchase> {
-    const { id, saleId, created_at, products } =
-      PurchaseMapper.toPersistence(purchase);
+    const { id, saleId, created_at } = PurchaseMapper.toPersistence(purchase);
 
-    const { createdPurchase, createdPurchaseProducts } =
-      await prisma.$transaction(async (trx) => {
-        const counter = await trx.purchaseCounter.findUnique({
-          where: { id: 1 },
-        });
+    const purchaseSaleProductInputs = purchasSaleProducts.map(
+      (purchaseSaleProduct) =>
+        PurchaseSaleProductMapper.toPersistence(purchaseSaleProduct),
+    );
 
-        if (!counter) {
-          await trx.purchaseCounter.create({
-            data: {
-              id: 1,
-              totalPurchase: 0,
-            },
-          });
-        }
-
-        const createdPurchase = await trx.purchase.create({
-          data: {
-            id,
-            saleId,
-            created_at,
-          },
-          include: {
-            sales: true,
-          },
-        });
-
-        await trx.purchaseCounter.update({
-          where: {
-            id: 1,
-          },
-          data: {
-            totalPurchase: {
-              increment: 1,
-            },
-          },
-        });
-
-        const productIds = products
-          .split(",")
-          .map((product: string) => product.trim());
-
-        const createdPurchaseProducts = await Promise.all(
-          productIds.map((productId) =>
-            trx.productPurchase.create({
-              data: {
-                id: randomUUID(),
-                productId,
-              },
-            }),
-          ),
-        );
-
-        await trx.sale.update({
-          where: { id: createdPurchase.saleId },
-          data: {
-            status: "Finalizada",
-          },
-        });
-
-        return { createdPurchase, createdPurchaseProducts };
+    const { createdPurchase } = await prisma.$transaction(async (trx) => {
+      const counter = await trx.purchaseCounter.findUnique({
+        where: { id: 1 },
       });
+
+      if (!counter) {
+        await trx.purchaseCounter.create({
+          data: {
+            id: 1,
+            totalPurchase: 0,
+          },
+        });
+      }
+
+      const createdPurchase = await trx.purchase.create({
+        data: {
+          id,
+          saleId,
+          created_at,
+        },
+        include: {
+          sale: true,
+        },
+      });
+
+      await trx.purchaseCounter.update({
+        where: { id: 1 },
+        data: {
+          totalPurchase: {
+            increment: 1,
+          },
+        },
+      });
+
+      await Promise.all(
+        purchaseSaleProductInputs.map(
+          async ({ id, purchaseId, saleProductId, created_at }) => {
+            return await trx.purchaseSaleProduct.create({
+              data: {
+                id,
+                purchaseId,
+                saleProductId,
+                created_at,
+              },
+            });
+          },
+        ),
+      );
+
+      await trx.sale.update({
+        where: { id: createdPurchase.saleId },
+        data: {
+          status: "Finalizada",
+        },
+      });
+
+      return { createdPurchase };
+    });
 
     return PurchaseMapper.toDomain({
       rawPrismaPurchase: createdPurchase,
-      rawPurchaseProducts: createdPurchaseProducts,
-      rawSales: createdPurchase.sales,
+      rawPrismaSale: createdPurchase.sale,
     });
   }
 
   async findMany(page: number, perPage: number): Promise<Purchase[]> {
     const purchaseOrPurchases = await prisma.purchase.findMany({
       include: {
-        sales: true,
+        sale: true,
+        purchaseSaleProducts: true,
       },
       orderBy: { created_at: "desc" },
       skip: (page - 1) * perPage,
@@ -95,9 +97,51 @@ export class PrismaPurchaseRepository implements PurchaseRepository {
     return purchaseOrPurchases.map((purchase) =>
       PurchaseMapper.toDomain({
         rawPrismaPurchase: purchase,
-        rawSales: purchase.sales,
+        rawPrismaSale: purchase.sale,
       }),
     );
+  }
+
+  // Create purchaseSaleProduct method to get products from purchase sale
+
+  async findPurchaseSaleProductByPurchaseId(
+    purchaseId: string,
+  ): Promise<Purchase | null> {
+    const purchaseOrNull = await prisma.purchase.findUnique({
+      where: { id: purchaseId },
+      include: {
+        purchaseSaleProducts: true,
+        sale: true,
+      },
+    });
+
+    if (purchaseOrNull === null) return null;
+
+    const purchaseSaleProducts = await Promise.all(
+      purchaseOrNull.purchaseSaleProducts.map(async (purchaseSaleProduct) => {
+        const saleProduct = await prisma.saleProduct.findUnique({
+          where: { id: purchaseSaleProduct.saleProductId },
+          include: {
+            product: true,
+            sale: true,
+          },
+        });
+
+        return {
+          purchaseSaleProduct,
+          saleProduct: saleProduct!,
+          product: saleProduct!.product,
+        };
+      }),
+    );
+
+    const purchaseMapper = PurchaseMapper.toDomain({
+      rawPrismaPurchase: purchaseOrNull,
+      rawPrismaSale: purchaseOrNull.sale,
+      rawPurchaseSaleProducts: purchaseSaleProducts,
+    });
+
+    return purchaseMapper;
   }
 
   async getTotalPurchasesCount(): Promise<number> {

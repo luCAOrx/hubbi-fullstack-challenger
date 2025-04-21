f9eca6c212170f3a105cd8795eb1ccc0e07fd451
luCAOrx <lucaorx@protonmail.com>
2025-04-20 23:15:24 -0300

    chore: Remove porducts from ToPersistenceResponse interface. Add PurchaseSaleProducts type. Remove rawPurchaseProducts. Change rawSale property name to rawPrismaSale. Add rawPrismaPurchaseSaleProducts property. Remove rawSales property from saleMapper. Add map to purchaseSaleProducts. Remove products property from DomainPurchase.create method. Add _sale. _purchaseSaleProducts, _id properties. Remove products from toPersistence method

--- server/src/infra/http/mappers/purchase-mapper.ts
+++ server/src/infra/http/mappers/purchase-mapper.ts
@@ -1,52 +1,70 @@
 import { Purchase as DomainPurchase } from "@domain/entities/purchase/purchase";
 import {
   Purchase as PrismaPurchase,
-  ProductPurchase,
+  PurchaseSaleProduct as PrismaPurchaseSaleProduct,
+  Product as PrismaProduct,
+  SaleProduct as PrismaSaleProduct,
   Sale as PrismaSale,
 } from "@prisma/client";
 
+import { PurchaseSaleProductMapper } from "./purchase-sale-product-mapper";
 import { SaleMapper } from "./sale-mapper";
 
 interface ToPersistenceResponse {
   id: string;
   saleId: string;
   created_at: Date;
-  products: string;
 }
+
+type PurchaseSaleProducts = {
+  purchaseSaleProduct: PrismaPurchaseSaleProduct;
+  saleProduct: PrismaSaleProduct;
+  product: PrismaProduct;
+};
 
 export class PurchaseMapper {
   static toDomain({
     rawPrismaPurchase,
-    rawPurchaseProducts,
-    rawSales,
+    rawPrismaSale,
+    rawPurchaseSaleProducts = [],
   }: {
     rawPrismaPurchase: PrismaPurchase;
-    rawPurchaseProducts?: ProductPurchase[];
-    rawSales: PrismaSale;
+    rawPrismaSale: PrismaSale;
+    rawPurchaseSaleProducts?: PurchaseSaleProducts[];
   }): DomainPurchase {
-    const saleMapper = SaleMapper.toDomain({ rawPrismaSale: rawSales });
+    const saleMapper = SaleMapper.toDomain({ rawPrismaSale });
+
+    const purchaseSaleProducts = rawPurchaseSaleProducts.map(
+      ({ purchaseSaleProduct, saleProduct, product }) =>
+        PurchaseSaleProductMapper.toDomain({
+          rawPrismaSale,
+          rawPrismaPurchase,
+          rawPrismaSaleProduct: saleProduct,
+          rawPrismaProduct: product,
+          rawPrismaPurchaseSaleProduct: purchaseSaleProduct,
+        }),
+    );
 
     return DomainPurchase.create(
       {
         saleId: rawPrismaPurchase.saleId,
-        products: String(
-          rawPurchaseProducts?.map((product) => product.productId).join(","),
-        ),
       },
-      rawPrismaPurchase.id,
-      saleMapper,
+      {
+        _sale: saleMapper,
+        _purchaseSaleProducts: purchaseSaleProducts,
+        _id: rawPrismaPurchase.id,
+      },
     );
   }
 
   static toPersistence({
     id,
-    props: { saleId, products },
+    props: { saleId },
     created_at,
   }: DomainPurchase): ToPersistenceResponse {
     return {
       id,
       saleId,
-      products,
       created_at,
     };
   }

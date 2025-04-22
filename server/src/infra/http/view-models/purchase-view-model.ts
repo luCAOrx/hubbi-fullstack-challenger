bdeba593ef56f2be1e973e60f6d9501c84da6b2f
luCAOrx <lucaorx@protonmail.com>
2025-04-22 00:34:17 -0300

    chore: Remove products property

--- server/src/infra/http/view-models/purchase-view-model.ts
+++ server/src/infra/http/view-models/purchase-view-model.ts
@@ -3,19 +3,17 @@ import { Purchase } from "@domain/entities/purchase/purchase";
 export interface CreatePurchaseToHttpResponse {
   id: string;
   saleId: string;
-  products: string;
   created_at: Date;
 }
 
 export class PurchaseViewModel {
   static toHttp({
     id,
-    props: { saleId, products },
+    props: { saleId },
     created_at,
   }: Purchase): CreatePurchaseToHttpResponse {
     return {
       id,
-      products,
       saleId,
       created_at,
     };

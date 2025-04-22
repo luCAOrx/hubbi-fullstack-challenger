b6d1a1ca63cea3c8653c38f4218965c28c8e1e68
luCAOrx <lucaorx@protonmail.com>
2025-04-22 06:36:40 -0300

    chore: Remove purchase sale product id column from sale-product table

---
+++ server/prisma/migrations/20250207003431_remove_purchase_sale_product_id_column_from_sale_product_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `purchaseSaleProductId` on the `saleProduct` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "saleProduct" DROP COLUMN "purchaseSaleProductId";

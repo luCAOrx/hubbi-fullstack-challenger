b2b6685dedacb50ae062e1744f883c3a521bef77
luCAOrx <lucaorx@protonmail.com>
2025-05-06 02:47:24 -0300

    docs: Add get sale product by id route

--- server/docs/swagger-paths.ts
+++ server/docs/swagger-paths.ts
@@ -5,6 +5,7 @@ import { createSaleRequest } from "./src/infra/http/routes/create-sale/create-sa
 import { getProductsRequest } from "./src/infra/http/routes/get-products/get-products-request";
 import { getPurhcaseSaleProductByPurchaseIdRequest } from "./src/infra/http/routes/get-purchase-sale-product-by-purchase-id/get-purchase-sale-product-by-purchase-id-request";
 import { getPurchasesRequest } from "./src/infra/http/routes/get-purchases/get-purchases-request";
+import { getSaleProductByIdRequest } from "./src/infra/http/routes/get-sale-product-by-id/get-sale-product-by-id-request";
 import { getSalesRequest } from "./src/infra/http/routes/get-sales/get-sales-request";
 import { pageNotFoundRequest } from "./src/infra/http/routes/global-responses/page-not-found-request";
 
@@ -15,6 +16,10 @@ export const swaggerPaths: Paths = {
 
   "/get-sales": {
     get: getSalesRequest,
+  },
+
+  "/get-sale-product-by-id/{saleId}": {
+    get: getSaleProductByIdRequest,
   },
 
   "/create-purchase/{saleId}": {

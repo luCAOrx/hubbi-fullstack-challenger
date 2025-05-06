0b29f5e47adfc3b777a24ec6d7270daf99346161
luCAOrx <lucaorx@protonmail.com>
2025-05-06 03:57:25 -0300

    test(E2E): Add query params to get-sale-product-by-id route, add comparisons to page, saleProductId and productName, add test to check if pagination are work

--- server/src/infra/http/controllers/get-sale-product-by-id/get-sale-product-by-id-controller.e2e-spec.ts
+++ server/src/infra/http/controllers/get-sale-product-by-id/get-sale-product-by-id-controller.e2e-spec.ts
@@ -1,4 +1,4 @@
-import { deepStrictEqual } from "node:assert";
+import { deepStrictEqual, notDeepStrictEqual } from "node:assert";
 import { describe, it } from "node:test";
 
 import { GetSaleProductByIdToHttpResponse } from "@infra/http/view-models/get-sale-product-by-id-view-model";
@@ -12,24 +12,75 @@ export function getSaleProductByIdControllerEndToEndTests(): void {
 
     it("should be able get sale product by id", async () => {
       await MakeRequestFactory.execute({
-        url: `${String(process.env.TEST_SERVER_URL)}/get-sale-product-by-id/${saleId[0]}`,
+        url: `${String(process.env.TEST_SERVER_URL)}/get-sale-product-by-id/${saleId[0]}?page=1`,
         method: "GET",
         headers,
       }).then(async (response) => {
-        const { products, saleId: id }: GetSaleProductByIdToHttpResponse =
+        const { data, page }: GetSaleProductByIdToHttpResponse =
           (await response.json()) as GetSaleProductByIdToHttpResponse;
 
         deepStrictEqual(response.status, 200);
-        deepStrictEqual(id, saleId[0]);
-        deepStrictEqual(products[0].productName, "Açaí com granola");
-        deepStrictEqual(products[1].productName, "Pão de queijo");
-        deepStrictEqual(products[2].productName, "Torta de frutas");
+        deepStrictEqual(page, 1);
+        deepStrictEqual(data.length, 10);
+        notDeepStrictEqual(data[0].saleProductId, data[1].saleProductId);
+        notDeepStrictEqual(data[1].saleProductId, data[2].saleProductId);
+        notDeepStrictEqual(data[2].saleProductId, data[3].saleProductId);
+        notDeepStrictEqual(data[3].saleProductId, data[4].saleProductId);
+        notDeepStrictEqual(data[4].saleProductId, data[5].saleProductId);
+        notDeepStrictEqual(data[5].saleProductId, data[6].saleProductId);
+        notDeepStrictEqual(data[6].saleProductId, data[7].saleProductId);
+        notDeepStrictEqual(data[7].saleProductId, data[8].saleProductId);
+        notDeepStrictEqual(data[8].saleProductId, data[9].saleProductId);
+        deepStrictEqual(data[0].productName, "Bolo de chocolate");
+        deepStrictEqual(data[1].productName, "Arroz integral");
+        deepStrictEqual(data[2].productName, "Feijão preto");
+        deepStrictEqual(data[3].productName, "Açaí com granola");
+        deepStrictEqual(data[4].productName, "Pão de queijo");
+        deepStrictEqual(data[5].productName, "Torta de frutas");
+        deepStrictEqual(data[6].productName, "Creme de leite");
+        deepStrictEqual(data[7].productName, "Leite condensado");
+        deepStrictEqual(data[8].productName, "Manteiga de amendoim");
+        deepStrictEqual(data[9].productName, "Coco ralado");
+      });
+    });
+
+    it("should be able jump to next page on sale product by id list", async () => {
+      await MakeRequestFactory.execute({
+        url: `${String(process.env.TEST_SERVER_URL)}/get-sale-product-by-id/${saleId[0]}?page=2`,
+        method: "GET",
+        headers,
+      }).then(async (response) => {
+        const { data, page }: GetSaleProductByIdToHttpResponse =
+          (await response.json()) as GetSaleProductByIdToHttpResponse;
+
+        deepStrictEqual(response.status, 200);
+        deepStrictEqual(page, 2);
+        deepStrictEqual(data.length, 10);
+        notDeepStrictEqual(data[0].saleProductId, data[1].saleProductId);
+        notDeepStrictEqual(data[1].saleProductId, data[2].saleProductId);
+        notDeepStrictEqual(data[2].saleProductId, data[3].saleProductId);
+        notDeepStrictEqual(data[3].saleProductId, data[4].saleProductId);
+        notDeepStrictEqual(data[4].saleProductId, data[5].saleProductId);
+        notDeepStrictEqual(data[5].saleProductId, data[6].saleProductId);
+        notDeepStrictEqual(data[6].saleProductId, data[7].saleProductId);
+        notDeepStrictEqual(data[7].saleProductId, data[8].saleProductId);
+        notDeepStrictEqual(data[8].saleProductId, data[9].saleProductId);
+        deepStrictEqual(data[0].productName, "Sabonete de limão");
+        deepStrictEqual(data[1].productName, "Shampoo de coco");
+        deepStrictEqual(data[2].productName, "Condizente para cabelo");
+        deepStrictEqual(data[3].productName, "Loção de barbear");
+        deepStrictEqual(data[4].productName, "Desodorante");
+        deepStrictEqual(data[5].productName, "Laca de unhas");
+        deepStrictEqual(data[6].productName, "Creme hidratante para pele");
+        deepStrictEqual(data[7].productName, "Kit de higiene bucal");
+        deepStrictEqual(data[8].productName, "Máscara para olhos");
+        deepStrictEqual(data[9].productName, "Sabão líquido");
       });
     });
 
     it("should not be able get sale product with sale inexistent", async () => {
       await MakeRequestFactory.execute({
-        url: `${String(process.env.TEST_SERVER_URL)}/get-sale-product-by-id/123456`,
+        url: `${String(process.env.TEST_SERVER_URL)}/get-sale-product-by-id/123456?page=1`,
         method: "GET",
         headers,
       }).then(async (response) => {
@@ -39,6 +90,24 @@ export function getSaleProductByIdControllerEndToEndTests(): void {
         deepStrictEqual(purchaseResponse, {
           statusCode: 400,
           message: "Venda não encontrada",
+          error: "Bad request",
+        });
+      });
+    });
+
+    it("should not be able get sale product without query params of the request", async () => {
+      await MakeRequestFactory.execute({
+        url: `${String(process.env.TEST_SERVER_URL)}/get-sale-product-by-id/${saleId[0]}`,
+        method: "GET",
+        headers,
+      }).then(async (response) => {
+        const responseBody = await response.json();
+
+        deepStrictEqual(response.status, 400);
+        deepStrictEqual(responseBody, {
+          statusCode: 400,
+          message:
+            "The query parameters: page must be provided in the query parameters of the request",
           error: "Bad request",
         });
       });

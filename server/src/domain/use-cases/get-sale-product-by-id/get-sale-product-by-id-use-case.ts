32709418a81bc2cb15ea77d0814e72ac232e5773
luCAOrx <lucaorx@protonmail.com>
2025-05-06 03:34:01 -0300

    chore: Add page property to GetSaleProductById request and response interface, change saleProduct property to data in GetSaleProductByIdResponse interface, add page with 1 value on execute method, add perPage variable, add variable to storage return from this.saleRepository.getSaleProductById method, change saleProduct object in return to page and data

--- server/src/domain/use-cases/get-sale-product-by-id/get-sale-product-by-id-use-case.ts
+++ server/src/domain/use-cases/get-sale-product-by-id/get-sale-product-by-id-use-case.ts
@@ -1,15 +1,17 @@
-import { Sale } from "@domain/entities/sale/sale";
+import { SaleProduct } from "@domain/entities/sale-product/sale-product";
 import { SaleRepository } from "@domain/repositories/sale-repository";
 
 import { BaseUseCase } from "../base-use-case";
 import { GlobalUseCaseErrors } from "../global-errors/global-use-case-errors";
 
 interface GetSaleProductByIdRequest {
+  page?: number;
   saleId: string;
 }
 
 export interface GetSaleProductByIdResponse {
-  saleProduct: Sale;
+  page: number;
+  data: SaleProduct[];
 }
 
 export class GetSaleProductByIdUseCase
@@ -18,14 +20,27 @@ export class GetSaleProductByIdUseCase
   constructor(private readonly saleRepository: SaleRepository) {}
 
   async execute({
+    page = 1,
     saleId,
   }: GetSaleProductByIdRequest): Promise<GetSaleProductByIdResponse> {
+    const perPage = 10;
+
     const saleOrNull = await this.saleRepository.findById(saleId);
 
     if (saleOrNull === null) {
       throw new GlobalUseCaseErrors.SaleNotFoundError();
     }
 
-    return { saleProduct: saleOrNull };
+    const saleProductOrSaleProducts =
+      await this.saleRepository.findSaleProductById(
+        saleOrNull.id,
+        page,
+        perPage,
+      );
+
+    return {
+      page,
+      data: saleProductOrSaleProducts,
+    };
   }
 }

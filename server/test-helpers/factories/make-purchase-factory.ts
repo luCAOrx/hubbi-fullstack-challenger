b30ba54e5dc9ce2d0ddabb326ccdb05e77d00bb2
luCAOrx <lucaorx@protonmail.com>
2025-05-11 00:29:37 -0300

    test(Helpers): Add 20 product ids and sale product ids

--- server/test-helpers/factories/make-purchase-factory.ts
+++ server/test-helpers/factories/make-purchase-factory.ts
@@ -44,7 +44,7 @@ export class MakePurchaseFactory {
         name: override?.saleName ?? "Create Purchase Test Unit",
         status: "Finalizada",
         products:
-          "1aec1cf9-3443-4e4a-a9c9-319967bfe74c,1bd59f2d-b6b8-4f63-acd9-068246b6fee5,2d1cf07f-617d-4aac-892c-6b26ceecf36f",
+          "27c7f671-539f-4bc7-9b2c-ce5e2b01680e,5e38a998-8f86-4d24-944a-72a93ec50bd2,2e48b04f-20be-4eb6-93a5-dde897b2b9f1,a0c64a5c-5d58-4b42-9cb1-6863f2157e5b,b7ff27ce-1266-4186-bbf7-05ccf674b1dd,6dab5309-4244-4aef-af48-f495416066a7,1fdaff11-3645-4760-915c-57d945234b71,5411452b-a587-4fe9-b7ff-9ccb5183a488,0280334f-9fdb-4ddc-91a2-7c1f8cdc9001,5212ad14-4390-424e-9bec-0bab7bc73bb1,65677067-fe4f-40d9-8b07-91d1540af036,e764e7a2-faca-4748-a576-2a5906f9e9c9,cad22725-b84e-4008-8117-a72152b7f7f4,724db296-9c01-4706-a711-2e6f9473d774,4e5d6e83-7682-4272-accf-32f918710210,c5ad7556-07ad-463d-bbeb-ea04e4a25100,bb41cbde-fda7-4879-a5ab-05b426e34a8d,5b8b536f-7adb-4797-9515-7036bba04606,4e92fd1d-5002-49eb-9b8a-f119e3ddfe19,a1ad812c-b4af-46c0-aa3f-01219e5a6401",
       },
       { _products: inMemorySaleDatabase.products },
     );
@@ -84,7 +84,7 @@ export class MakePurchaseFactory {
 
     const { purchase } = await createPurchaseUseCase.execute({
       saleId: saleId ?? sale.id,
-      saleProductId: `${inMemorySaleDatabase.saleProducts[0].id},${inMemorySaleDatabase.saleProducts[1].id},${inMemorySaleDatabase.saleProducts[2].id}`,
+      saleProductId: `${inMemorySaleDatabase.saleProducts[0].id},${inMemorySaleDatabase.saleProducts[1].id},${inMemorySaleDatabase.saleProducts[2].id},${inMemorySaleDatabase.saleProducts[3].id},${inMemorySaleDatabase.saleProducts[4].id},${inMemorySaleDatabase.saleProducts[5].id},${inMemorySaleDatabase.saleProducts[6].id},${inMemorySaleDatabase.saleProducts[7].id},${inMemorySaleDatabase.saleProducts[8].id},${inMemorySaleDatabase.saleProducts[9].id},${inMemorySaleDatabase.saleProducts[10].id},${inMemorySaleDatabase.saleProducts[11].id},${inMemorySaleDatabase.saleProducts[12].id},${inMemorySaleDatabase.saleProducts[13].id},${inMemorySaleDatabase.saleProducts[14].id},${inMemorySaleDatabase.saleProducts[15].id},${inMemorySaleDatabase.saleProducts[16].id},${inMemorySaleDatabase.saleProducts[17].id},${inMemorySaleDatabase.saleProducts[18].id},${inMemorySaleDatabase.saleProducts[19].id}`,
       ...override,
     });
 
@@ -131,7 +131,7 @@ export class MakePurchaseFactory {
       method: "POST",
       headers,
       data: {
-        saleProductId: `${saleProducts[0].id},${saleProducts[1].id},${saleProducts[2].id}`,
+        saleProductId: `${saleProducts[0].id},${saleProducts[1].id},${saleProducts[2].id},${saleProducts[3].id},${saleProducts[4].id},${saleProducts[5].id},${saleProducts[6].id},${saleProducts[7].id},${saleProducts[8].id},${saleProducts[9].id},${saleProducts[10].id},${saleProducts[11].id},${saleProducts[12].id},${saleProducts[13].id},${saleProducts[14].id},${saleProducts[15].id},${saleProducts[16].id},${saleProducts[17].id},${saleProducts[18].id},${saleProducts[19].id}`,
         ...override,
       },
     });

ec8ff91f47c983860c70813e68aa365f040d1e51
luCAOrx <lucaorx@protonmail.com>
2025-05-23 05:09:38 -0300

    feat: Add get products controller

--- server/src/infra/http/controllers/get-products/get-products-controller.ts
+++ server/src/infra/http/controllers/get-products/get-products-controller.ts
@@ -1,27 +1,51 @@
 import { Request, Response } from "express";
 
-import { prisma } from "@infra/http/libs/prisma-client";
+import { GetProductsUseCase } from "@domain/use-cases/get-products/get-products-use-case";
+import { PrismaProductRepository } from "@infra/http/repositories/prisma-product-repository";
+import { GetProductsViewModel } from "@infra/http/view-models/get-products-view-model";
 
 import { BaseController } from "../base-controller";
+
+interface GetProductsQueryParamsProps {
+  page: string;
+}
 
 export class GetProductsController extends BaseController {
   protected async executeImplementation(
     request: Request,
     response: Response,
   ): Promise<any> {
-    await prisma.product
-      .findMany({})
-      .then((productOrProducts) => {
-        return this.ok({
-          response,
-          message: { productOrProducts },
+    const { page } = request.query as unknown as GetProductsQueryParamsProps;
+
+    const prismaProductRepository = new PrismaProductRepository();
+
+    const getProductsUseCase = new GetProductsUseCase(prismaProductRepository);
+
+    await getProductsUseCase
+      .execute({ page: Number(page) })
+      .then(({ page, perPage, pages, totalProducts, data }) => {
+        const message = GetProductsViewModel.toHttp({
+          page,
+          perPage,
+          pages,
+          totalProducts,
+          data,
         });
+
+        return this.ok({ response, message });
       })
-      .catch((error: Error) => {
-        return this.clientError({
-          response,
-          message: "Erro ao listar produtos",
-        });
+      .catch(() => {
+        if (
+          Object.keys(request.query).length === 0 ||
+          Object.hasOwn(request.query, "page") ||
+          !Object.hasOwn(request.query, "page")
+        ) {
+          return this.clientError({
+            response,
+            message:
+              "The query parameters: page must be provided in the query parameters of the request",
+          });
+        }
       });
   }
 }

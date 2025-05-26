f62d5bef13334548d9369d166e645cdabb588e3a
luCAOrx <lucaorx@protonmail.com>
2025-05-26 07:05:59 -0300

    feat: Add searchParams feature. Add Header component. Add Tabs component to Sales and Purchases component

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,53 @@
-export default function Home() {
+import Header from "@/components/header/header";
+import { Main } from "@/components/main";
+import Purchases from "@/components/main/contents/purchases/purchases";
+import PurchasesSkeleton from "@/components/main/contents/purchases/purchases-skeleton";
+import Sales from "@/components/main/contents/sales/sales";
+import SalesSkeleton from "@/components/main/contents/sales/sales-skeleton";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Suspense } from "react";
+
+type SearchParamsProps = Promise<{
+  vendaPagina: number;
+  compraPagina: number;
+}>;
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: SearchParamsProps;
+}) {
+  const { vendaPagina, compraPagina } = (await searchParams) || 1;
+
   return (
-    <h1 className="font-medium  bg-zinc-800 text-gray-100 text-lg">
-      Hello World
-    </h1>
+    <>
+      <Header />
+      <div className="flex justify-center">
+        <Tabs defaultValue="sales">
+          <TabsList>
+            <TabsTrigger value="sales">Vendas</TabsTrigger>
+            <TabsTrigger value="purchases">Compras</TabsTrigger>
+          </TabsList>
+          <TabsContent value="sales">
+            <Main.Root>
+              <Main.Content>
+                <Suspense fallback={<SalesSkeleton />}>
+                  <Sales saleSearchParam={vendaPagina} />
+                </Suspense>
+              </Main.Content>
+            </Main.Root>
+          </TabsContent>
+          <TabsContent value="purchases">
+            <Main.Root>
+              <Main.Content>
+                <Suspense fallback={<PurchasesSkeleton />}>
+                  <Purchases purchaseSearchParam={compraPagina} />
+                </Suspense>
+              </Main.Content>
+            </Main.Root>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
   );
 }

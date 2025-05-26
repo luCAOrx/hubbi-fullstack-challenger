1b9af6e31cee09961d03320b6a376409375e4149
luCAOrx <lucaorx@protonmail.com>
2025-05-26 07:13:07 -0300

    feat: Add LoadingButton component

---
+++ web/src/components/loading-button.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Button } from "./ui/button";
+import { Loader2 } from "lucide-react";
+
+interface ButtonLoadingProps {
+  isSubmitting: boolean;
+}
+
+export default function LoadingButton({ isSubmitting }: ButtonLoadingProps) {
+  return (
+    <Button type="submit" disabled={isSubmitting}>
+      {isSubmitting ? (
+        <div className="inline-flex items-center justify-center h-9 w-[61px]">
+          <Loader2 className="animate-spin" />
+        </div>
+      ) : (
+        "Cadastrar"
+      )}
+    </Button>
+  );
+}

b811be11e12a6aeab12b0b5953c629710422d210
luCAOrx <lucaorx@protonmail.com>
2025-05-26 07:16:32 -0300

    feat: Add PurchaseDetailsCardButton component

---
+++ web/src/components/main/contents/purchases/purchase-details-card-button.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import InfiniteScroll from "@/components/infinite-scroll";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { getPurchaseSaleProductByPurchaseId } from "@api/get-purchase-sale-product-by-purchase-id";
+import { Loader2 } from "lucide-react";
+import React, { useState } from "react";
+
+interface PurchaseSaleProduct {
+  purchaseSaleProductId: string;
+  productName: string;
+  saleName: string;
+}
+
+interface PurchaseDetailsCardButtonProps {
+  purchaseId: string;
+  saleName: string;
+}
+
+export default function PurchaseDetailsCardButton({
+  purchaseId,
+  saleName,
+}: PurchaseDetailsCardButtonProps) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [purchaseSaleProducts, setPurchaseSaleProducts] = useState<
+    PurchaseSaleProduct[]
+  >([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+
+  async function handleGetPurchaseSaleProductByPurchaseId() {
+    await getPurchaseSaleProductByPurchaseId({ purchaseId }, { page }).then(
+      (response) => {
+        setPurchaseSaleProducts(response.data);
+        setPage(page + 1);
+
+        if (response.data.length < 10) {
+          setHasMore(false);
+        }
+        setLoading(false);
+      },
+    );
+
+    setPurchaseSaleProducts((prev) => [...purchaseSaleProducts, ...prev]);
+  }
+
+  return (
+    <>
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogTrigger
+          asChild
+          onClick={() => {
+            handleGetPurchaseSaleProductByPurchaseId();
+            setIsDialogOpen(true);
+          }}
+        >
+          <Button>Ver detalhes</Button>
+        </DialogTrigger>
+        <DialogContent className="flex flex-col items-start">
+          <DialogHeader>
+            <DialogTitle>Detalhes da compra</DialogTitle>
+            <DialogDescription>
+              Detalhes da compra da venda: {saleName}
+            </DialogDescription>
+          </DialogHeader>
+          <h1 className="font-semibold">
+            {purchaseSaleProducts.length > 1
+              ? "Foram comprados os produtos:"
+              : "Foi comprado o produto:"}
+          </h1>
+          <ScrollArea className="h-48 w-64 rounded-md border">
+            {purchaseSaleProducts.map(
+              ({ purchaseSaleProductId, productName }) => (
+                <div className="mx-3 my-2" key={purchaseSaleProductId}>
+                  <span className="font-sm text-foreground">{productName}</span>
+                </div>
+              ),
+            )}
+            <InfiniteScroll
+              hasMore={hasMore}
+              isLoading={loading}
+              next={handleGetPurchaseSaleProductByPurchaseId}
+              threshold={1}
+            >
+              {hasMore && (
+                <Loader2 className="my-4 mx-20 h-8 w-8 animate-spin" />
+              )}
+            </InfiniteScroll>
+          </ScrollArea>
+
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Fechar</Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}

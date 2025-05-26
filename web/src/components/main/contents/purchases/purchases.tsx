909a4a406710067e03c45c17918e9ddc6573ec87
luCAOrx <lucaorx@protonmail.com>
2025-05-26 07:17:47 -0300

    feat: Add Purchases component

---
+++ web/src/components/main/contents/purchases/purchases.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../../../ui/table";
+import { getPurchases } from "@api/get-purchases";
+import { Pagination } from "@/components/pagination";
+import { formatDateHelper } from "@/helpers/format-date-helper";
+import PurchaseDetailsCardButton from "./purchase-details-card-button";
+
+interface PurchasesProps {
+  purchaseSearchParam: number;
+}
+
+export default async function Purchases({
+  purchaseSearchParam,
+}: PurchasesProps) {
+  const purchases = await getPurchases({ page: purchaseSearchParam });
+
+  return (
+    <>
+      <div className="flex justify-between mb-4">
+        <div className="flex flex-col">
+          <h1 className="font-semibold text-4xl text-primary mb-4">Compras</h1>
+          <span className="text-lg font-normal text-muted-foreground">
+            Total de {purchases.totalPurchases} compras
+          </span>
+        </div>
+      </div>
+      <div className="border rounded-lg p-4 mb-10">
+        <Table>
+          {purchases.data.length === 0 ? (
+            <TableCaption>Não há nenhuma compra no momento.</TableCaption>
+          ) : (
+            <TableCaption>Uma lista de suas compras.</TableCaption>
+          )}
+          <TableHeader>
+            {purchases.data.length === 0 ? (
+              <TableRow className="hover:bg-transparent">
+                <TableHead className="w-[380px]">Nome da venda</TableHead>
+                <TableHead className="w-52">Criado em</TableHead>
+                <TableHead className="pl-10">Ações</TableHead>
+              </TableRow>
+            ) : (
+              <TableRow className="hover:bg-transparent">
+                <TableHead className="w-[380px]">Nome da venda</TableHead>
+                <TableHead className="w-52">Criado em</TableHead>
+                <TableHead className="pl-10 w-36">Ações</TableHead>
+              </TableRow>
+            )}
+          </TableHeader>
+          <TableBody>
+            {purchases.data.map(
+              ({ purchaseId, saleName, purchase_created_at }) => (
+                <TableRow key={purchaseId} className="hover:bg-transparent">
+                  <TableCell className="w-[180px]">{saleName}</TableCell>
+                  <TableCell className="w-36">
+                    {formatDateHelper(purchase_created_at)}
+                  </TableCell>
+                  <TableCell className="w-36">
+                    <PurchaseDetailsCardButton
+                      purchaseId={purchaseId}
+                      saleName={saleName}
+                    />
+                  </TableCell>
+                </TableRow>
+              ),
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <Pagination
+        perPage={purchases.perPage}
+        pages={purchases.pages}
+        totalCount={purchases.totalPurchases}
+        queryParamName="compraPagina"
+      />
+    </>
+  );
+}
